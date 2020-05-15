d7bcb1cb0e6fc291331efc476574efd82b02dc5d
Tevvin <Tevvin7395@icloud.com>
2020-05-15 00:07:41 -0400

    input added for user input from players

--- src/adv.py
+++ src/adv.py
@@ -38,8 +38,8 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player(room['outside'])
-print(player)
+player = Player("Tevvin", room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -50,3 +50,9 @@ print(player)
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    print(player.room.name)
+    print(player.room.description)
+
+    user = input("Which way, Player? ")
